04af721ad03127cbbd80f048b020655949ac478b
olivergirard <girard1@purdue.edu>
2022-08-11 20:08:31 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using utauPlugin;
+using UtauVoiceBank;
+using Wave;
+
+namespace MassEnvelopeEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UtauPlugin utauPlugin = new UtauPlugin(args[0]);
+            utauPlugin.Input();
+            foreach (Note note in utauPlugin.note)
+            {
+                note.SetNoteNum(note.GetNoteNum() + 1);
+            }
+            utauPlugin.Output();
+        }
+    }
+}
