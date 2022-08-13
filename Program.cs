e80ddebd78431b89c00d78855edfbee83f1631f1
olivergirard <girard1@purdue.edu>
2022-08-13 16:50:30 -0400

    allowed user to select a range

--- Program.cs
+++ Program.cs
@@ -11,12 +11,38 @@ namespace MassEnvelopeEditor
             UtauPlugin utauPlugin = new UtauPlugin(args[0]);
             utauPlugin.Input();
 
+            Note firstNote = new Note();
+            bool copiedEnvelope = false;
+
             foreach (Note note in utauPlugin.note)
             {
-                note.SetEnvelope((utauPlugin.note[0]).GetEnvelope());
+                /* if the note is part of a selected range */
+
+                if (IsSelected(note) == true)
+                {
+                    if (copiedEnvelope == false)
+                    {
+                        firstNote = note.Next;
+                        copiedEnvelope = true;
+                    } else
+                    {
+                        note.SetEnvelope(firstNote.GetEnvelope());
+                    }
+                }
             }
 
             utauPlugin.Output();
+        }
+        static bool IsSelected(Note note)
+        {
+            if (note.GetRegion() != null)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+
         }
     }
 }
